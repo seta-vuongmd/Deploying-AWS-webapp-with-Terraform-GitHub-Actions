510f6390d35f9d5287d7458afca54e303b5fc72e
seta-vuongmd-7141 <vuong.mai@seta-international.vn>
2025-06-10 01:25:33 +0700

    update AWS region to ap-southeast-2 in workflows and Terraform configuration

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "tfstate-for-locking" # Created & Versioning Enabled Manually.
     key            = "terraform.tfstate"   # path and name of state file.
-    region         = "eu-south-1"
+    region         = "ap-southeast-2"
     dynamodb_table = "state_table" # name of dynamodb table for State Lock, must have partition key = "LockID"
     # encrypt = true # by default
   }
--- Terraform/prod_values.auto.tfvars
+++ Terraform/prod_values.auto.tfvars
@@ -1,4 +1,4 @@
-region = "eu-south-1"
+region = "ap-southeast-2"
 
 #-------------Networking Variables Default Values-------------------#
 
