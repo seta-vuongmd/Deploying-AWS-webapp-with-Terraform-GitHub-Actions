51fc106045f8e1bb17e4cd5faa4110f7632d0989
seta-vuongmd-7141 <vuong.mai@seta-international.vn>
2025-06-10 01:30:23 +0700

    update S3 bucket name in Terraform backend configuration

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tfstate-for-locking" # Created & Versioning Enabled Manually.
+    bucket         = "devops-directive-tf-state-my" # Created & Versioning Enabled Manually.
     key            = "terraform.tfstate"   # path and name of state file.
     region         = "ap-southeast-2"
     dynamodb_table = "state_table" # name of dynamodb table for State Lock, must have partition key = "LockID"
